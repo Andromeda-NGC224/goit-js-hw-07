1dc194669304d6381a5c7a993ad4f0d6316f1d7d
Andromeda-NGC224 <shadim115115@gmail.com>
2024-01-25 17:35:28 +0200

    12
    12

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,18 @@
 `use strict`;
 
+const widget = document.querySelector(`.widget`);
+const changeBtn = document.querySelector(`.change-color`);
+const spanColor = document.querySelector(`.color`);
+const body = document.querySelector(`body`);
+
+changeBtn.addEventListener(`click`, handleClick);
+
+function handleClick(event) {
+  spanColor.textContent = "- ";
+  spanColor.insertAdjacentHTML("beforeend", getRandomHexColor());
+  body.style.background = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
